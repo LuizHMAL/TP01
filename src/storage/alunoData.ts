8302c1ead166e88e44bfe4d05f2d98807a0ac839
Luiz Henrique Meira <leite.luizhenrique94@gmail.com>
2025-11-15 15:04:24 -0300

    Relatório adicionado ao PDF

--- src/storage/alunoData.ts
+++ src/storage/alunoData.ts
@@ -1,26 +1,26 @@
-import { Aluno } from '../model/Aluno.js';
-import { writeFile, readFile } from "fs/promises";
+// import { Aluno } from '../model/Aluno.js';
+// import { writeFile, readFile } from "fs/promises";
 
 
 
-const FILE_PATH = 'alunos.dat';
-const encoding = 'utf-8';
+// const FILE_PATH = 'alunos.dat';
+// const encoding = 'utf-8';
 
-export async function saveAlunos(alunos: Aluno | Aluno[]) {
-	const data = JSON.stringify(alunos, null, 2);
-	try {
-		await writeFile(FILE_PATH, data, { encoding });
-	} catch (err) {
-		throw err;
-	}
-}
+// export async function saveAlunos(alunos: Aluno | Aluno[]) {
+// 	const data = JSON.stringify(alunos, null, 2);
+// 	try {
+// 		await writeFile(FILE_PATH, data, { encoding });
+// 	} catch (err) {
+// 		throw err;
+// 	}
+// }
 
-export async function readAlunos(): Promise<Aluno[] | null> {
-	try {
-		const content = await readFile(FILE_PATH, { encoding });
-		return JSON.parse(content) as Aluno[];
-	} catch (err) {
-		return null;
-	}
-}
+// export async function readAlunos(): Promise<Aluno[] | null> {
+// 	try {
+// 		const content = await readFile(FILE_PATH, { encoding });
+// 		return JSON.parse(content) as Aluno[];
+// 	} catch (err) {
+// 		return null;
+// 	}
+// }
 
